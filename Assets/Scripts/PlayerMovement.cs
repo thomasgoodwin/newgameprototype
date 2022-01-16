5737fe821fb1a541bd168c682bfb579c7de066db
Ott <>
2022-01-16 13:52:07 -0800

    added bhopping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,20 +8,33 @@ public class PlayerMovement : MonoBehaviour
     public Transform cam;
 
     public float speed = 3f;
+    public float acceleration = 10f;
     public float friction = 0.01f;
+    public float stopSpeed = 0.1f;
+    public float jumpVelocity = 2f;
 
     public float turnSmoothTime = .1f;
     float turnSmoothVelocity;
     Vector3 velocity;
     public float gravity = -9.81f;
 
+    private bool isOnGround = false;
+
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+        float jump = Input.GetAxisRaw("Jump");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         velocity.y += gravity * Time.deltaTime;
+
+        if (jump > 0f && isOnGround)
+        {
+            velocity.y = jumpVelocity;
+            isOnGround = false;
+            Debug.Log("jump");
+        }
 
         if (direction.magnitude >= 0.1f)
         {
@@ -30,21 +43,48 @@ public class PlayerMovement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 wishDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            velocity += wishDir;
-            velocity *= 1 - friction;
-            float alreadySpeed = Vector3.Dot(wishDir.normalized, velocity.normalized);
+            float alreadySpeed = Vector3.Dot(wishDir, velocity);
+            Debug.Log("alreadySpeed:" + alreadySpeed);
+            if (speed > alreadySpeed)
+            {
+                velocity += wishDir * Mathf.Min(speed - alreadySpeed, acceleration * Time.deltaTime * friction);
+            }
+            
+            if (isOnGround)
+            {
+                velocity.y = 0;
+                Friction();
+            }
 
             if (alreadySpeed > speed)
             {
                 velocity *= speed / alreadySpeed;
             }
-            
-            characterController.Move(velocity * Time.deltaTime);
+
+            CollisionFlags flags = characterController.Move(velocity * Time.deltaTime + (isOnGround ? Vector3.down * 0.01f : Vector3.zero));
+            isOnGround = flags.HasFlag(CollisionFlags.Below);
         }
-        else if (velocity.magnitude >= 0.1f)
+        else// if (velocity.magnitude >= stopSpeed)
         {
-            velocity *= 1 - friction;
-            characterController.Move(velocity * Time.deltaTime);
+            if (isOnGround)
+            {
+                velocity.y = 0;
+                Friction();
+            }
+            CollisionFlags flags = characterController.Move(velocity * Time.deltaTime + (isOnGround ? Vector3.down * 0.01f : Vector3.zero));
+            isOnGround = flags.HasFlag(CollisionFlags.Below);
+        }
+        
+    }
+    void Friction()
+    {
+        float curSpeed = velocity.magnitude;
+        float control = Mathf.Max(speed, stopSpeed);
+        float drop = control * friction * Time.deltaTime;
+        float newSpeed = Mathf.Max(0, curSpeed - drop);
+        if (newSpeed != curSpeed)
+        {
+            velocity *= newSpeed / curSpeed;
         }
     }
 }
